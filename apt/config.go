69ce5cfeacb61370fee8b2524a637e645ed6c794
tonyxia2023 <tony@movemnt.dev>
2024-04-13 02:22:18 +0000

    update node config

--- apt/config.go
+++ apt/config.go
@@ -3,8 +3,8 @@ package apt
 // var Url = "https://api.testnet.aptoslabs.com/v1"
 var IndexerUrl = "https://indexer-testnet.staging.gcp.aptosdev.com/v1/graphql"
 var SupportChain = map[string]string{
-	"apt1": "http://18.217.97.143:8080/v1",
-	"evm1": "http://18.217.97.143:8080/v1",
-	"apt2": "http://18.222.148.169:8080/v1",
-	"evm2": "http://18.222.148.169:8080/v1",
+	"apt1": "https://rollup2.bbd.sh/v1",
+	"evm1": "https://rollup2.bbd.sh/v1",
+	"apt2": "https://rollup1.bbd.sh/v1",
+	"evm2": "https://rollup1.bbd.sh/v1",
 }
