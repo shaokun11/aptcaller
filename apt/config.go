bc7aeb9ebb7f408fc7251ce8238e705d06e57971
tonyxia2023 <tony@movemnt.dev>
2024-04-15 13:56:04 +0000

    update apt call config url

--- apt/config.go
+++ apt/config.go
@@ -3,8 +3,8 @@ package apt
 // var Url = "https://api.testnet.aptoslabs.com/v1"
 var IndexerUrl = "https://indexer-testnet.staging.gcp.aptosdev.com/v1/graphql"
 var SupportChain = map[string]string{
-	"apt1": "https://rollup1.bbd.sh/v1",
-	"evm1": "https://rollup1.bbd.sh/v1",
-	"apt2": "https://rollup2.bbd.sh/v1",
-	"evm2": "https://rollup2.bbd.sh/v1",
+	"apt1": "https://api.internal.test1.m1.movementlabs.xyz/v1",
+	"evm1": "https://api.internal.test1.m1.movementlabs.xyz/v1",
+	"apt2": "https://api.internal.test2.m1.movementlabs.xyz/v1",
+	"evm2": "https://api.internal.test2.m1.movementlabs.xyz/v1",
 }
